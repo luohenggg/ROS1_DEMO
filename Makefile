88d80220c08167dce0b418c86371be7cc3338d1f
骆恒 <heng.luo@ubtrobot.com>
2022-08-17 15:37:23 +0800

    add ignore

---
+++ Makefile
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+clean:
+	rm -rf demo03_ws/build
+	rm -rf demo03_ws/install
+	rm -rf demo03_ws/devel
+	rm -rf demo02_ws/build
+	rm -rf demo02_ws/install
+	rm -rf demo02_ws/devel
+	rm -rf demo_ws/build
+	rm -rf demo_ws/install
+	rm -rf demo_ws/devel
--- demo03_ws/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lh/work/ROS_demo_ws_XuZuoZhao/demo03_ws/src
-# Build directory: /home/lh/work/ROS_demo_ws_XuZuoZhao/demo03_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("urdf01_rviz")
-subdirs("urdf_gazebo")
